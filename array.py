24b1a75624325e061910ffb384188863bb7c4cdb
nisarahmad78 <talk2nisar78@gmail.com>
2025-07-11 18:38:57 +0500

    pushing array

--- array.py
+++ array.py
@@ -10,6 +10,13 @@ arr1 = np.array([1, 2, 3])
 #2D Array
 arr2 = np.array([[1, 2], [3, 4]])
 
+#Special Arrays
+np.zeros((2, 3))     # 2x3 array of zeros
+np.ones((3, 3))      # 3x3 array of ones
+np.eye(3)            # 3x3 Identity matrix
+np.arange(0, 10, 2)  # [0, 2, 4, 6, 8]
+np.linspace(0, 1, 5) # 5 points between 0 and 1
+
 
 
 
