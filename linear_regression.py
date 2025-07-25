5c7402f1a23045cdbc20423ad03d2090b554d149
nisarahmad78 <talk2nisar78@gmail.com>
2025-07-25 12:19:28 +0500

    Polynomial_regression.

--- linear_regression.py
+++ linear_regression.py
@@ -4,9 +4,10 @@ from scipy import stats
 X = [6,8,10,12,14,18,20]
 Y = [300,700,1200,1600,2200,2700,3500]
 Slope, intercept , r, p , std_error = stats.linregress(X,Y)
-def slopes(x):
-    return Slope*x +intercept
-model = list(map(slopes,X ))
-plt.scatter(X,Y)
-plt.plot(X,model)
-plt.show()
+# def slopes(x):
+#     return Slope*x +intercept
+# model = list(map(slopes,X ))
+# plt.scatter(X,Y)
+# plt.plot(X,model)
+# plt.show()
+print(r)
