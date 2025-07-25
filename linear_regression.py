99eb362d5fcb18aee05b1973ea7376b9fe9e6a2d
nisarahmad78 <talk2nisar78@gmail.com>
2025-07-25 11:40:08 +0500

    regression

---
+++ linear_regression.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+X = [6,8,10,12,14,18,20]
+Y = [300,700,1200,1600,2200,2700,3500]
+Slope, intercept , r, p , std_error = stats.linregress(X,Y)
+def slopes(x):
+    return Slope*x +intercept
+model = list(map(slopes,X ))
+plt.scatter(X,Y)
+plt.plot(X,model)
+plt.show()
---
+++ scatterPlot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x = [5,7,8,7,2,17,2,17,29,4,11,12,96]
+y = [99,86,87,88,11,86,103,87,94,78,77,85,86]
+plt.scatter(x,y)
+plt.show()
