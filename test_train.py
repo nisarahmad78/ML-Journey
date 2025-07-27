9622d3dc58a10627ceef07c9f36352b32005c52a
nisarahmad78 <talk2nisar78@gmail.com>
2025-07-27 11:46:57 +0500

    test and train data

---
+++ test_train.py
@@ -0,0 +1,12 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# np.random.seed(2)
+# plot_x = np.random.normal(3,1,100)
+# plot_y = np.random.normal(150,40,100)/plot_x
+# plt.scatter(plot_x,plot_y)
+# plt.show()
+
+
+test_array = [10,11,12,13,14,15,10,20,1]
+print(test_array[0:5:2])
+print(test_array[5:])
