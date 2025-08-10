fddf24544db89ce121cb9c844ccf4a378d62eb10
nisarahmad78 <talk2nisar78@gmail.com>
2025-08-10 17:13:59 +0500

    data Preprocessing

---
+++ Data_Science_Project/mobile_screen_time_analysis.py
@@ -0,0 +1,5 @@
+'''Screen Time Analysis is the task of analyzing and creating a report
+on which applications and websites are used by the user for how much 
+times. Apply deices have on of the best ways of creawting a screen
+time report.
+'''
